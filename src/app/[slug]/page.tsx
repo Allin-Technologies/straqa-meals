217e8e6a3b52f5faca1a040408ccd03416e8c153
kingsonseang <kingsonseang@gmail.com>
2025-02-15 14:12:26 +0100

    ols

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
-import { getMenuBySlug } from "@/actions/get-by-slug";
+import { getDepricatedMenuBySlug } from "@/actions/get-by-slug";
 import { getServerSideURL } from "@/utilities/getURL";
-// import { PageClientDepricated } from "./page.client";
+import { PageClientDepricated } from "./page.client";
 import { Sofadi_One } from "next/font/google";
 import { cn } from "@/utilities/ui";
 import {
@@ -29,102 +29,104 @@ type Props = {
 export default async function Page({ params }: Props) {
   const slug = (await params).slug;
 
-  const data = await getMenuBySlug(slug);
+  const data = await getDepricatedMenuBySlug(slug);
 
   if (!data) notFound();
 
-  console.log(data?.content?.items);
-
   return (
-    <div
-      style={
-        {
-          // background: `url(${data?.content?.background && typeof data?.content?.background !== "string" ? data?.content?.background?.url : fallbackbg})`,
-          // background: `url(${fallbackbg})`,
-        }
-      }
-      className='container w-full p-4 lg:p-8 flex flex-col justify-center items-center min-h-dvh'
-    >
-      <div className='pt-24 flex justify-center items-center min-h-[30vh] w-full'>
-        <h1 className={cn("text-8xl", sofadiOne.className)}>{data?.title}</h1>
-      </div>
-
-      <div
-        className='w-full py-4 lg:py-8 space-y-8 rounded-[54px] flex-1 bg-white/20 backdrop-blur-md'
-        style={
-          {
-            // background: `url(${data?.content?.background && typeof data?.content?.background !== "string" ? data?.content?.background?.url : fallbackbg})`,
-            // background: `url(${fallbackbg})`,
-          }
-        }
-      >
-        {data?.logo && typeof data?.logo !== "string" && data?.logo?.url && (
-          <div className='px-4 lg:px-8'>
-            <Image
-              src={data?.logo?.url}
-              alt={data?.logo?.alt ?? ""}
-              width={100}
-              height={100}
-              className='object-contain w-14'
-            />
-          </div>
-        )}
-
-        <Accordion type='single'>
-          {data?.content?.items?.map((item, index) => (
-            <AccordionItem
-              key={item.id || index}
-              value={item.id || index?.toString()}
-              className={cn({
-                "border-b-transparent":
-                  data?.content?.items?.length == index + 1,
-              })}
-            >
-              <AccordionTrigger
-                // style={{
-                //   border: `0 0 4px ${item.color}`,
-                //   // filter: `drop-shadow(3px 0 0 ${item.color}) drop-shadow(0 3px 0 ${item.color}) drop-shadow(-3px 0 0 ${item.color}) drop-shadow(0 -3px 0 ${item.color})`,
-                // }}
-                className={cn(
-                  "font-bold text-3xl px-4 lg:px-8 py-6",
-                  sofadiOne.className
-                )}
-              >
-                {item?.label}
-              </AccordionTrigger>
-              <AccordionContent
-                // style={{
-                //   background: item.color,
-                // }}
-                className='px-4 lg:px-8 pb-6 space-y-3'
-              >
-                {item?.items?.map((item) => (
-                  <div key={item.id || index} className={sofadiOne.className}>
-                    <div className='flex justify-between gap-3 text-xl'>
-                      <p>{item?.title}</p>
-                      <div className='flex-1 border-white border-b border-dotted mb-3' />
-                      <p>
-                        {item?.price.currency}{" "}
-                        {item?.price.amount?.toLocaleString()}
-                      </p>
-                    </div>
-                    {item?.subtitle && (
-                      <div>
-                        <p>{item?.subtitle}</p>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </div>
+    <div className='container w-full p-4 lg:p-8 flex flex-col justify-center items-center min-h-dvh'>
+      <PageClientDepricated {...data} />
     </div>
   );
-}
 
-// {/* <PageClientDepricated {...data} /> */}
+  // return (
+  //   <div
+  //     style={
+  //       {
+  //         // background: `url(${data?.content?.background && typeof data?.content?.background !== "string" ? data?.content?.background?.url : fallbackbg})`,
+  //         // background: `url(${fallbackbg})`,
+  //       }
+  //     }
+  //     className='container w-full p-4 lg:p-8 flex flex-col justify-center items-center min-h-dvh'
+  //   >
+  //     <div className='pt-24 flex justify-center items-center min-h-[30vh] w-full'>
+  //       <h1 className={cn("text-8xl", sofadiOne.className)}>{data?.title}</h1>
+  //     </div>
+
+  //     <div
+  //       className='w-full py-4 lg:py-8 space-y-8 rounded-[54px] flex-1 bg-white/20 backdrop-blur-md'
+  //       style={
+  //         {
+  //           // background: `url(${data?.content?.background && typeof data?.content?.background !== "string" ? data?.content?.background?.url : fallbackbg})`,
+  //           // background: `url(${fallbackbg})`,
+  //         }
+  //       }
+  //     >
+  //       {data?.logo && typeof data?.logo !== "string" && data?.logo?.url && (
+  //         <div className='px-4 lg:px-8'>
+  //           <Image
+  //             src={data?.logo?.url}
+  //             alt={data?.logo?.alt ?? ""}
+  //             width={100}
+  //             height={100}
+  //             className='object-contain w-14'
+  //           />
+  //         </div>
+  //       )}
+
+  //       <Accordion type='single'>
+  //         {data?.content?.items?.map((item, index) => (
+  //           <AccordionItem
+  //             key={item.id || index}
+  //             value={item.id || index?.toString()}
+  //             className={cn({
+  //               "border-b-transparent":
+  //                 data?.content?.items?.length == index + 1,
+  //             })}
+  //           >
+  //             <AccordionTrigger
+  //               // style={{
+  //               //   border: `0 0 4px ${item.color}`,
+  //               //   // filter: `drop-shadow(3px 0 0 ${item.color}) drop-shadow(0 3px 0 ${item.color}) drop-shadow(-3px 0 0 ${item.color}) drop-shadow(0 -3px 0 ${item.color})`,
+  //               // }}
+  //               className={cn(
+  //                 "font-bold text-3xl px-4 lg:px-8 py-6",
+  //                 sofadiOne.className
+  //               )}
+  //             >
+  //               {item?.label}
+  //             </AccordionTrigger>
+  //             <AccordionContent
+  //               // style={{
+  //               //   background: item.color,
+  //               // }}
+  //               className='px-4 lg:px-8 pb-6 space-y-3'
+  //             >
+  //               {item?.items?.map((item) => (
+  //                 <div key={item.id || index} className={sofadiOne.className}>
+  //                   <div className='flex justify-between gap-3 text-xl'>
+  //                     <p>{item?.title}</p>
+  //                     <div className='flex-1 border-white border-b border-dotted mb-3' />
+  //                     <p>
+  //                       {item?.price.currency}{" "}
+  //                       {item?.price.amount?.toLocaleString()}
+  //                     </p>
+  //                   </div>
+  //                   {item?.subtitle && (
+  //                     <div>
+  //                       <p>{item?.subtitle}</p>
+  //                     </div>
+  //                   )}
+  //                 </div>
+  //               ))}
+  //             </AccordionContent>
+  //           </AccordionItem>
+  //         ))}
+  //       </Accordion>
+  //     </div>
+  //   </div>
+  // );
+}
 
 export async function generateMetadata(
   { params }: Props,
@@ -132,7 +134,7 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const slug = (await params).slug;
 
-  const data = await getMenuBySlug(slug);
+  const data = await getDepricatedMenuBySlug(slug);
 
   if (!data) {
     return {};
